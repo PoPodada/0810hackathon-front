256605717b74ce02e0348a6e6801f4f11e1d0a84
PoPodada <goito.722@gmail.com>
2025-08-03 21:53:09 +0900

    feat: マップページの作成

--- src/app/map.tsx
+++ src/app/map.tsx
@@ -1,55 +1,67 @@
 import { useState } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 import Slider from "@react-native-community/slider";
 import MapView, {
   PROVIDER_GOOGLE,
   Region,
   Marker,
   Circle,
+  LatLng,
+  MapMarkerProps,
+  MarkerDragEvent,
 } from "react-native-maps";
+import { Link } from "expo-router";
 
 const API_KEY = "";
 
-const RegionValue: Region = {
+const INITIAL_COORDINATE: LatLng = {
   latitude: 34.3845479077209,
   longitude: 132.45614910237433,
+};
+
+const RegionValue: Region = {
+  ...INITIAL_COORDINATE,
   latitudeDelta: 0.0922,
   longitudeDelta: 0.0421,
 };
 
+const INITIAL_RADIUS = 1000;
+
 const Page = () => {
-  const [radius, setRadius] = useState(43000);
-  const [markerCoordinate, setMarkerCoordinate] = useState({
-    latitude: 34.3845479077209,
-    longitude: 132.45614910237433,
-  });
+  const [radius, setRadius] = useState(INITIAL_RADIUS);
+  const [center, setCenter] = useState(INITIAL_COORDINATE);
+
+  const handleMarkerDrag = (e: MarkerDragEvent) => {
+    const { coordinate } = e.nativeEvent;
+    setCenter(coordinate);
+  };
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Link href="/mode">戻る</Link>
+      </View>
       <MapView
         style={styles.map}
         provider={PROVIDER_GOOGLE}
         initialRegion={RegionValue}
       >
-        <Marker
-          draggable
-          onDragEnd={(e) => setMarkerCoordinate(e.nativeEvent.coordinate)}
-          coordinate={markerCoordinate}
-        />
-        <Circle center={markerCoordinate} radius={radius} strokeWidth={5} />
+        <Marker draggable onDrag={handleMarkerDrag} coordinate={center} />
+        <Circle center={center} radius={radius} strokeWidth={5} />
       </MapView>
       <View style={styles.footer}>
         <Slider
           style={{ width: 200, height: 40 }}
           minimumValue={0}
           maximumValue={2000}
-          minimumTrackTintColor="#FFFFFF"
+          minimumTrackTintColor="#000000"
           maximumTrackTintColor="#000000"
           onValueChange={(value) => setRadius(value)}
         />
+        <Button title="決定" />
         <Text>{radius}</Text>
         <Text>
-          {markerCoordinate.latitude}, {markerCoordinate.longitude}
+          {center.latitude}, {center.longitude}
         </Text>
       </View>
     </View>
@@ -57,6 +69,14 @@ const Page = () => {
 };
 
 const styles = StyleSheet.create({
+  header: {
+    width: "100%",
+    height: 60,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingLeft: 20,
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
@@ -65,46 +85,11 @@ const styles = StyleSheet.create({
   },
   map: {
     width: "100%",
-    height: "100%",
+    height: "80%",
   },
   footer: {
-    height: 200,
+    height: "10%",
   },
 });
 
 export default Page;
-
-{
-  /* <APIProvider apiKey={API_KEY}>
-          <Map
-            defaultCenter={INITIAL_CENTER}
-            defaultZoom={10}
-            gestureHandling={"greedy"}
-            disableDefaultUI={true}
-          >
-            <Marker
-              position={center}
-              draggable
-              onDrag={(e) =>
-                setCenter({
-                  lat: e.latLng?.lat() ?? 0,
-                  lng: e.latLng?.lng() ?? 0,
-                })
-              }
-            />
-            <Circle
-              radius={radius}
-              center={center}
-              onRadiusChanged={setRadius}
-              onCenterChanged={changeCenter}
-              strokeColor={"#0c4cb3"}
-              strokeOpacity={1}
-              strokeWeight={3}
-              fillColor={"#3b82f6"}
-              fillOpacity={0.3}
-              editable
-              draggable
-            />
-          </Map>
-        </APIProvider> */
-}
